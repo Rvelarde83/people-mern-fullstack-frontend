e2f343fbd3134f96fc341f29d9dc61da4d315238
Ramon Velarde <rvelarde83@hotmail.com>
2022-05-25 19:04:32 -0700

    Added basic components and boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div className="App">
+   <h1>Ramono!</h1>
+   <Header />
+   <Main />
+     
+    </div>
+  );
+}
+
+export default App;
